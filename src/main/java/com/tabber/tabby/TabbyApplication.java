4c6c4a207580957cc13c941a319c830a3e6a360a
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-11-29 03:38:57 +0530

    added env files

--- src/main/java/com/tabber/tabby/TabbyApplication.java
+++ src/main/java/com/tabber/tabby/TabbyApplication.java
@@ -1,16 +1,22 @@
-package com.example.tabby;
+package com.tabber.tabby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@EnableAsync
+@EnableCaching
+@EnableScheduling
 public class TabbyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabbyApplication.class, args);
+		System.out.println("Tabby is up");
 	}
 
 }
--- src/test/java/com/tabber/tabby/TabbyApplicationTests.java
+++ src/test/java/com/tabber/tabby/TabbyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tabby;
+package com.tabber.tabby;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
