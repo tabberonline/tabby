9d9ad7480ad53384eaa8592f899eaed7b2aeb8d2
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-11-29 00:23:03 +0530

    f commit

---
+++ src/main/java/com/example/tabby/TabbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabbyApplication.class, args);
+	}
+
+}
