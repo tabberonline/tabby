fc3cb962e10b4c42f5afc1c541d657269824764f
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-12-02 16:53:07 +0530

    refactor pom

--- src/main/java/com/tabber/tabby/controllers/AuthController.java
+++ src/main/java/com/tabber/tabby/controllers/AuthController.java
@@ -1,6 +1,5 @@
 package com.tabber.tabby.controllers;
 
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.tabber.tabby.security.JWTService;
