ad05e6a4b3f278a572e96047ddc7b17d74b848ee
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-12-19 01:40:49 +0530

    affed email

--- src/main/java/com/tabber/tabby/controllers/HealthController.java
+++ src/main/java/com/tabber/tabby/controllers/HealthController.java
@@ -1,5 +1,7 @@
 package com.tabber.tabby.controllers;
 
+import com.tabber.tabby.email.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    @Autowired
+    EmailService emailService;
     @GetMapping(value = "ping")
     public ResponseEntity<String> ping(){
+        emailService.sendMail("mandeep.sidhu2@gmail.com","test","lo lol lo l");
         return new ResponseEntity<>("Pong", HttpStatus.OK);
     }
 }
