05f867687a1b158f07b346714e645848eadb654f
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2021-03-06 01:16:29 +0530

    logging

--- src/main/java/com/tabber/tabby/email/EmailService.java
+++ src/main/java/com/tabber/tabby/email/EmailService.java
@@ -1,36 +1,13 @@
 package com.tabber.tabby.email;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.tabber.tabby.entity.EmailEntity;
-import com.tabber.tabby.entity.UserEntity;
-import com.tabber.tabby.manager.EmailsManager;
-import com.tabber.tabby.manager.UserResumeManager;
-import com.tabber.tabby.respository.EmailsRepository;
-import com.tabber.tabby.respository.UserRepository;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.HtmlUtils;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-
 import javax.mail.internet.MimeMessage;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @Service
@@ -39,12 +16,16 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    private static final Logger logger = Logger.getLogger(EmailService.class.getName());
+
+
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     public void sendMail(String toEmail, String subject, String message) {
         MimeMessage mailMessage = javaMailSender.createMimeMessage();
+        logger.log(Level.INFO,"Sending email");
         try {
             mailMessage.setSubject(subject, "UTF-8");
             MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true,StandardCharsets.UTF_8.name());
@@ -54,6 +35,7 @@ public class EmailService {
             javaMailSender.send(mailMessage);
         }
         catch (Exception ex){
+            logger.log(Level.WARNING,"Failed in sending email on contact page, due to "+ex.toString());
         }
     }
 
