7f38d5ce0f2127b4b3d111f0d28076a08ba86ccd
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2021-03-05 12:16:22 +0530

    correct cron

--- src/main/java/com/tabber/tabby/configuration/ConfigureQuartzJob.java
+++ src/main/java/com/tabber/tabby/configuration/ConfigureQuartzJob.java
@@ -21,7 +21,7 @@ public class ConfigureQuartzJob {
         return TriggerBuilder.newTrigger().forJob(emailingProfileReceiverJob)
                 .withIdentity("emailJobTrigger")
                 //.withSchedule(CronScheduleBuilder.cronSchedule("0 0 12 1/1 * ? *")) // everyday
-                .withSchedule(CronScheduleBuilder.cronSchedule("30 * * * *")) // every half hour
+                .withSchedule(CronScheduleBuilder.cronSchedule("*/30 * * * *")) // every half hour
                 .build();
     }
 }
